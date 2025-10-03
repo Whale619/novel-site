25d6e695b4f676c0141d30c6fa0d83af68dabf09
whale619 <huagui419@gmail.com>
2025-10-03 22:56:11 +0800

    更新內容

--- parser.py
+++ parser.py
@@ -41,6 +41,7 @@ def clean_line(line: str) -> str | None:
     line = line.replace("李松白", "李松柏")
     line = line.replace("九派一房", "九派一幫")
     line = line.replace("白千", "白天")
+    line = line.replace("私塾", "師叔")
     line = line.replace("!", "！")
     line = line.replace("?", "？")
 
